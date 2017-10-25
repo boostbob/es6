03fa4385353fbd4840602c5bdfa31e9e9fd309ed
Bob Wang <wangbo@yiqijiao.cn>
2017-10-25 14:20:37 +0800

    save

---
+++ express-next-implements.js
@@ -0,0 +1,40 @@
+const app = {
+  middleware: [],
+  callback(ctx) {
+    console.log(ctx);
+  },
+  use(fn) {
+    this.middleware.push(fn);
+  },
+  go(ctx) {
+    console.error(this.middleware, this.middleware.length);
+    // console.error(this.callback.toString());
+    var func = this.middleware.reduceRight((next, fn, index) => {
+      console.error(index, '=>', next.toString());
+      return () => fn(ctx, next);
+    // }, this.callback.bind(this, ctx));
+    // initial(the first next)
+    }, function() {console.error('start')});
+    
+    func();
+  }
+};
+
+app.use(function(ctx, next) {
+  console.error('1');
+  ctx.name = 'lucy';
+  next();
+});
+
+app.use(function(ctx, next) {
+  console.error('2');
+  ctx.age = 12;
+  next();
+});
+
+app.use(function(ctx, next) {
+  console.log(`${ctx.name} is ${ctx.age} years old`);
+  next();
+});
+
+app.go({});
--- flatten.js
+++ flatten.js
@@ -1,2 +1,11 @@
 var _ = require('underscore');
 
+// flatten 合并多个数组会有问题? 写写法问题!!!
+console.error(_.flatten(0, [1, 2])); // []
+console.error(_.flatten([0], [1, 2])); // [ 0 ]
+
+// 注意参数值是1个素组
+console.error(_.flatten([1, [2], [3, [[4]]]]));
+
+// shallow mode: the array will only be flattened a single level
+console.error(_.flatten([1, [2], [3, [[4]]]], true));
