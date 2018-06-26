f81a919f158cda9de5f80ed3d479e16e23f94cb8
Bob Wang <wangbo@yiqijiao.cn>
2018-06-26 16:54:11 +0800

    save

--- inquirer.js
+++ inquirer.js
@@ -1,14 +1,23 @@
 const inquirer = require('inquirer');
 
 async function main() {
-  let answers = await inquirer.prompt([
+  let answers;
+  
+  answers = await inquirer.prompt([
+    {
+      name: 'action',
+      type: 'confirm'
+    }
+  ]);
+  
+  answers = await inquirer.prompt([
     {
       name: 'action',
       type: 'list',
       message: 'Can i do help you?',
       choices: ['wait', 'break', 'exit']
     }
-  ])
+  ]);
 }
 
 main();
